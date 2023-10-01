4eed29e7087c4ac38e57537f3079187975f6655b
Zerron21 <geomertyaxe21@gmail.com>
2023-10-01 22:44:40 +0100

    Added P4G Support
    P4G support should be running properly and automatically.

--- prebuild.bat
+++ prebuild.bat
@@ -1,24 +1,29 @@
-:: Variable settings, Change only the FINALIZE value to 0 if you want to keep the used files for recompilation
+:: Variable settings, Change FINALIZE to 0 to keep loose audio files after the BGM file is compiled.
+:: Make sure loose audio files are in a folder named "AUDIO" inside of the usual .acb location.
 set EDITOR_PATH=.\.derp\SonicAudioTools\AcbEditor.exe
-set /A FINALIZE=1
+set /A FINALIZE=0
 
-:: Platform check.
-if exist .\BASE (set BGM_PATH=\BASE\SOUND& set PLATFORM=Switch) else set BGM_PATH=\SOUND& set PLATFORM=PS4
-set BGM_ORIGIN=..\..\..\Original\Persona 5 Royal (%PLATFORM%)%BGM_PATH%
+for %%t in (.\..) do set GAME="%%~nt"
+set GAME=%GAME:"=%
 
-:: Necessity check & error exists.
-if exist .%BGM_PATH%\BGM.ACB if exist .%BGM_PATH%\BGM.AWB exit
-:: if not exist "%BGM_ORIGIN%\BGM.ACB" if not exist "%BGM_ORIGIN%\BGM.AWB" exit
+if exist .\sound\adx2\bgm (set BGM_PATH=sound\adx2\bgm& goto Origin_Setting)
+if exist .\BASE (set BGM_PATH=BASE\SOUND& goto Origin_Setting) else set BGM_PATH=SOUND
+
+:Origin_Setting
+set BGM_ORIGIN=..\..\..\Original\%GAME%\%BGM_PATH%
+
+:: Necessity check & error exits.
+if exist .\%BGM_PATH%\*BGM.ACB if exist .\%BGM_PATH%\*BGM*.AWB exit
 
 :: Create BGM copies, unpack them, replace tracks from BGM folder, repack and rename appropriately.
-for %%F in ("%BGM_ORIGIN%\BGM.A?B") do copy "%%F" .%BGM_PATH%\BESTCITYRUAN%%~xF
-start /W /MIN %EDITOR_PATH% ".%BGM_PATH%\BESTCITYRUAN.ACB"
-copy .%BGM_PATH%\BGM\00???_streaming.??? .%BGM_PATH%\BESTCITYRUAN\00???_streaming.???
-start /W /MIN %EDITOR_PATH% ".%BGM_PATH%\BESTCITYRUAN"
-del /Q .%BGM_PATH%\BESTCITYRUAN && rmdir .%BGM_PATH%\BESTCITYRUAN
-ren .%BGM_PATH%\BESTCITYRUAN.A?B BGM.A?B
+for %%F in ("%BGM_ORIGIN%\*BGM*.A?B") do copy "%%F" .\%BGM_PATH%\*%%~xF
+for %%B in (".\%BGM_PATH%\*.ACB") do start /W /MIN %EDITOR_PATH% %%B & set BGM_NAME=%%~nB
+
+copy .\%BGM_PATH%\audio\?????_streaming.??? .\%BGM_PATH%\%BGM_NAME%\?????_streaming.???
+start /W /MIN %EDITOR_PATH% ".\%BGM_PATH%\%BGM_NAME%"
+del /Q .\%BGM_PATH%\%BGM_NAME% && rmdir .\%BGM_PATH%\%BGM_NAME%
 if %FINALIZE% NEQ 1 exit
 
 :: Finalize and delete unnecessary files, cannot be undone.
 del /Q .\.derp\SonicAudioTools && rmdir .\.derp\SonicAudioTools && rmdir .\.derp
-del /Q .%BGM_PATH%\BGM && rmdir .%BGM_PATH%\BGM && del /Q Ignore.aem
+del /Q .\%BGM_PATH%\AUDIO && rmdir .\%BGM_PATH%\AUDIO && del /Q Ignore.aem
